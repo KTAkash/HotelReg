d068fe961da63a3b33ef09059127e11470de17ea
Akash <subenakash2004@gmail.com>
2025-05-02 17:04:10 +0530

    first change

--- vehicle/src/components/EmployeeView.js
+++ vehicle/src/components/EmployeeView.js
@@ -33,7 +33,7 @@ const Emplview = () => {
         <Container className="mt-5">
             <Row>
                 <Col>
-                    <h1 className="text-center">Employees</h1>
+                    <h1 className="text-center">Staffs</h1>
                     <Table striped bordered hover responsive>
                         <thead>
                             <tr>
